d1ceaebd743675255344bf77332989fb7c6df55b
Braxton Hathaway <braxtonhathaway@Braxtons-MacBook-Air.local>
2017-06-08 10:13:09 -0400

    commit one

---
+++ main.js
@@ -0,0 +1,34 @@
+function () {
+   'use strict';
+
+  window.setTimeout(function(){ alert("Hello"); }, 3000);
+console.log(setTimeout);
+
+
+
+console.log(currentOnPage);
+
+console.log(currentPerSec);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}());
